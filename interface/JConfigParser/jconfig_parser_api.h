1396c3a3d213b0f94ae071478a158d0f1175cce9
王政乔 <me@zhengqiao.wang>
2022-12-15 10:16:53 +0800

    添加any的ut

--- interface/JConfigParser/jconfig_parser_api.h
+++ interface/JConfigParser/jconfig_parser_api.h
@@ -8,49 +8,6 @@ namespace Joger
     namespace ConfigParser
     {
         
-
-        
-
-    /*
-     *
-     {
-        "h1":"abc",
-        "h2":{
-            "h21":"123",
-            "h22":[1,2,3],
-            "h23":["1","2","3"]
-        },
-        "h3":[
-            {
-                "h311":"123",
-                "h312":4321
-            },
-            {
-                "h321":"aaa",
-                "h322":234.2
-            }
-        ]
-     } 
-     map{
-        value,
-        map{
-            value,
-            vec(value),
-            vec(value)
-        },
-        vec(
-            map{
-                value,
-                value
-            },
-            map{
-                value,
-                value
-            }
-        )
-     }
-     * 
-     */
     }
 }
 
---
+++ ut/model/any.cpp
@@ -0,0 +1,38 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "JConfigParser/model/any.h"
+
+using namespace Joger::Model;
+
+TEST_CASE("test Any basic type")
+{
+    Any a;
+    CHECK(a.isNull());
+    a = 123;
+    CHECK(a.isNull() == false);
+    CHECK(a.is<int>());
+    a = 123.4;
+    CHECK(a.is<double>());
+    CHECK_THROWS_AS(a.cast<int>(), std::bad_cast);
+}
+
+TEST_CASE("test Any c11 types")
+{
+    Any a(std::string("abc"));
+    CHECK(a.is<std::string>());
+
+    Any b(a);
+    CHECK(b.is<std::string>());
+
+    Any c = std::vector<int>{1, 2, 3};
+    CHECK(c.is<std::vector<int>>());
+    auto &c1 = c.cast<std::vector<int>>();
+    c1.emplace_back(4);
+
+    int i = 1;
+    for (auto &item : c.cast<std::vector<int>>())
+    {
+        CHECK(item == i);
+        ++i;
+    }
+}
