998e2cc41d9179468a0ac418dea4f4203c4005fe
王政乔 <me@zhengqiao.wang>
2022-12-16 18:26:39 +0800

    添加前后对比，验证支持json5

--- example/test.cpp
+++ example/test.cpp
@@ -8,7 +8,8 @@ using namespace Joger::ConfigParser;
 int main(int argc, char const *argv[])
 {
     JsonDecoder jc;
-    std::string txt = "{ // feqn \n \"h1\":\"a\\\"bc\",\n\"h2\":{ //feqddddw\n    \"h21\":\"123\",\n    \"h22\":[1,\n    2,\n    3], //fee\n    \"h23\":[\"1\"\n    ,\"2\"\n    ,\"3\"]\n},h3:[\n    { // hihihi\n        \"h311\":\"123\",\n        \"h312\":4321\n    },\n    {\n        \"h321\":\"aaa\",\n        \"h322\":234.2\n    }\n]\n}";
-    printf("%s\n",jc.decode(txt)->toString().c_str());
+    std::string txt = "{ // feqn \n \"h1\":\"a\\\"bc\",\n\"h2\":{ //feqddddw\n    \"h21\":\"123\",\n    \"h22\":[1,\n    2,\n    3], //fee\n    \"h23\":[\"1\"\n    ,\"2\"\n    ,\"3\"]\n},h3:[\n    { // hihihi\n        \"h311\":\"123\",\n        \"h312\":4321 ,\n    },\n    {\n        \"h321\":\"aaa\",\n        \"h322\":234.2\n    }\n]\n}";
+    printf("--------> before:\n%s\n", txt.c_str());
+    printf("--------> after:\n%s\n", jc.decode(txt)->toString().c_str());
     return 0;
 }
